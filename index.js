af5fe3d823164292187b37a628f7fa68313cc2df
TheFactoryProject <63594174+Cristovk@users.noreply.github.com>
2023-01-05 11:28:32 -0300

    coment

---
+++ index.js
@@ -0,0 +1,12 @@
+// dos nuevas rutas pra Pms y Oxigeno, para guardar de forma masiva los datos, lo primero que se necesita es leer el array de objetos que viene cons 
+// la data y luego guardarlos en la base de datos
+
+// export const timeLabe = Array.from({ length: 30 }, (_, i) => {
+// object 
+//   return {
+//     id: i,
+//     fecha: '2021-05-01',
+//     hora: '00:00:00',
+//    timestamp: 1620000000,
+//    name: 'Sensor'
+// }
